f52c48a0d10907836a87d9e5b930781e6122c6d3
ifeeljoy <psalm2517@outlook.com>
2025-03-17 00:15:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ const splashTexts = [
   "Welcome to the Pi Tracker site!",
   "Keep calm and mine Pi.",
   "Your Pi is ready for you.",
-  "Have you voted today?"
+  "Have you voted today?",
+  "Mining Pi, one slice at a time.",
+  "Crypto made easy!",
+  "Pi is the new black.",
+  "More than just a number!",
+  "Pi Day is every day!",
+  "Pi-ing since 2019.",
+  "In Pi we trust.",
+  "The future is decentralized!"
 ];
 
 function getRandomSplashText() {
